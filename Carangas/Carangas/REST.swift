308265cb4e24fc10a515b035b74d3f98a2c87594
Clara Nascimento <clara.nascimento@outlook.com>
2021-05-26 20:25:45 -0300

    141.1 - Create class Car.

---
+++ Carangas/Carangas/REST.swift
@@ -0,0 +1,13 @@
+//
+//  REST.swift
+//  Carangas
+//
+//  Created by Clara Nascimento on 26/05/21.
+//  Copyright © 2021 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+class REST {
+    
+}
