48832ac3453d689197189f1cd3dde99a4604faf2
Clara Nascimento <clara.nascimento@outlook.com>
2021-05-26 20:40:53 -0300

    141.2 - Create class REST; add private static let session, add private static let configuration, add private static let basePath.

--- Carangas/Carangas/REST.swift
+++ Carangas/Carangas/REST.swift
@@ -10,4 +10,20 @@ import Foundation
 
 class REST {
     
+    private static let basePath = "https://carangas.herokuapp.com/cars"
+    
+    private static let configuration: URLSessionConfiguration = {
+        let config = URLSessionConfiguration.default
+        config.allowsCellularAccess = false
+        config.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        config.timeoutIntervalForRequest = 30.0
+        config.httpMaximumConnectionsPerHost = 5
+        return config
+    } ()
+    
+    private static let session = URLSession(configuration: configuration)    //URLSession.shared
+    
+    
+    
+    
 }
