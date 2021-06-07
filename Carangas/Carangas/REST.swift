f67f9a1266af5917cf541617dd1c4fbd11583313
Clara Nascimento <clara.nascimento@outlook.com>
2021-06-07 20:09:59 -0300

    142.2 - Create class func loadCars

--- Carangas/Carangas/REST.swift
+++ Carangas/Carangas/REST.swift
@@ -23,7 +23,29 @@ class REST {
     
     private static let session = URLSession(configuration: configuration)    //URLSession.shared
     
-    
-    
-    
+    class func loadCars() {
+        guard let url = URL(string: basePath) else {return}
+        
+        let dataTask = session.dataTask(with: url) {(data: Data?, response: URLResponse?, error: Error?) in
+            
+            if error == nil {
+                
+                guard let response = response as? HTTPURLResponse else {return}
+                if response.statusCode == 200 {
+                    
+                    guard let data = data else {return}
+                    
+                    
+                } else {
+                    print("Algum status inválido pelo servidor!!")
+                }
+                
+            } else {
+                print(error!)
+            }
+
+        }
+        dataTask.resume()
+        
+    }
 }
