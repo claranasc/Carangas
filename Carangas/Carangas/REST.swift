7d5d30f074d4afecab29edb981b04daf69e39b57
Clara Nascimento <clara.nascimento@outlook.com>
2021-06-16 22:10:43 -0300

    142.5 - Add enum CarErro; Add class func loadCars; Add  var cars: [Car]; Add override func viewWillAppear; Add override func tableView.

--- Carangas/Carangas/REST.swift
+++ Carangas/Carangas/REST.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+enum CarError {
+    case url
+    case taskError(error: Error)
+    case noResponse
+    case noData
+    case responseStatusCode(code: Int)
+    case invalidJSON
+}
+
 class REST {
     
     private static let basePath = "https://carangas.herokuapp.com/cars"
@@ -23,26 +32,35 @@ class REST {
     
     private static let session = URLSession(configuration: configuration)    //URLSession.shared
     
-    class func loadCars() {
-        guard let url = URL(string: basePath) else {return}
+    class func loadCars(onComplete: @escaping ([Car]) -> Void, onError: @escaping (CarError) -> Void) {
+        guard let url = URL(string: basePath) else {
+            onError(.url)
+            return
+        }
+        
         let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
             if error == nil {
-                guard let response = response as? HTTPURLResponse else {return}
+                guard let response = response as? HTTPURLResponse else {
+                    onError(.noResponse)
+                    return
+                }
+                
                 if response.statusCode == 200 {
                     guard let data = data else {return}
                     do {
                         let cars = try JSONDecoder().decode([Car].self, from: data)
-                        for car in cars {
-                            print(car.name, car.brand)
-                        }
+                        onComplete(cars)
+                        
                     } catch {
-                        print(error.localizedDescription)
+                        //print(error.localizedDescription)
+                        onError(.invalidJSON)
                     }
                 } else {
-                    print("Algum status inválido pelo servidor!!")
+                    //print("Algum status inválido pelo servidor!!")
+                    onError(.responseStatusCode(code: response.statusCode))
                 }
             } else {
-                print(error!)
+                onError(.taskError(error: error!))
             }
         }
         dataTask.resume()
