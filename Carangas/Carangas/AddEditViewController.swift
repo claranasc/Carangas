72bce7bf30bdc7882e2d72f179f83d7b387b87c3
Clara Nascimento <clara.nascimento@outlook.com>
2021-06-30 22:36:02 -0300

    143.2 - Add var car, add @IBAction func addEdit.

--- Carangas/Carangas/AddEditViewController.swift
+++ Carangas/Carangas/AddEditViewController.swift
@@ -17,7 +17,10 @@ class AddEditViewController: UIViewController {
     @IBOutlet weak var scGasType: UISegmentedControl!
     @IBOutlet weak var btAddEdit: UIButton!
     @IBOutlet weak var loading: UIActivityIndicatorView!
-
+    
+    // MARK: - Properties
+    var car: Car!
+    
     // MARK: - Super Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,26 @@ class AddEditViewController: UIViewController {
     
     // MARK: - IBActions
     @IBAction func addEdit(_ sender: UIButton) {
+        if car == nil {
+            car = Car()
+        }
+        car.name = tfName.text!
+        car.brand = tfBrand.text!
+        if tfPrice.text!.isEmpty {tfPrice.text = "0"}
+        car.price = Double(tfPrice.text!)!
+        //car.price = Double(tfPrice.text ?? "0")!
+        car.gasType = scGasType.selectedSegmentIndex
+        
+        REST.save(car: car) { (success) in
+            self.goBack()
+        }
     }
-
+    
+    // MARK: - Methods
+    
+    func goBack() {
+        DispatchQueue.main.async {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
 }
